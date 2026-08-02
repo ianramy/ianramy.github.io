8c7aaa543286f8489347c73450ba57a750934b72
Ian Ramy <imwagore@gmail.com>
2026-08-02 21:14:47 +0300

    chore(deps): update biome and added serwist turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,35 @@
-import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
-
-const withSerwist = withSerwistInit({
-	swSrc: "src/app/sw.ts",
-	swDest: "public/sw.js",
-	disable: process.env.NODE_ENV !== "production",
-});
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants";
 
 const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	turbopack: {},
-	output: "export",
-	images: {
-		unoptimized: true,
-	},
+  reactStrictMode: true,
+  turbopack: {},
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default withSerwist(nextConfig);
+export default async (phase: string): Promise<NextConfig> => {
+  // Only inject Serwist during the dev server and production build phases
+  if (
+    phase === PHASE_DEVELOPMENT_SERVER ||
+    phase === PHASE_PRODUCTION_BUILD
+  ) {
+    // Dynamically import Serwist to prevent Turbopack conflicts
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "src/app/sw.ts",
+      swDest: "public/sw.js",
+      // Disables Serwist in development to prevent caching issues,
+      // but enables it for the production build
+      disable: phase === PHASE_DEVELOPMENT_SERVER,
+    });
+
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
+};
